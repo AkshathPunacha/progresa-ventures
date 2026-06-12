4a3f7f84ea6b913b0cba84648490534d4de9e9fe
Your Name <akshath.punacha@gmail.com>
2026-06-12 20:57:45 +0530

    Fix cropping of green cards on mobile devices

--- src/App.jsx
+++ src/App.jsx
@@ -459,7 +459,7 @@ function Services() {
                   animate={{ opacity: 1, y: 0 }}
                   exit={{ opacity: 0, y: -16 }}
                   transition={{ duration: 0.5, ease: [0.22, 1, 0.36, 1] }}
-                  className="absolute inset-0 p-8 lg:p-12 flex flex-col justify-between"
+                  className="relative lg:absolute lg:inset-0 p-6 sm:p-8 lg:p-12 flex flex-col justify-between"
                 >
                   <div>
                     <div className="flex items-center gap-3 text-xs tracking-[0.25em] uppercase text-sage-light">
